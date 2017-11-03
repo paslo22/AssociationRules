e92d71e7a69df1f19226139fddc90ac39ca8f0c9
paslo <paslo22@gmail.com>
2017-11-03 11:50:46 -0300

    Adding Celery settings

--- associationRules/settings.py
+++ associationRules/settings.py
@@ -125,3 +125,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 MEDIA_URL = '/media/'
+
+
+# CELERY SETTINGS
+
+BROKER_URL = 'redis://localhost:6379/0'
+
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+CELERY_ACCEPT_CONTENT = ['json']
+
+CELERY_TASK_SERIALIZER = 'json'
+
+CELERY_RESULT_SERIALIZER = 'json'
+
+CELERY_IGNORE_RESULT = False
